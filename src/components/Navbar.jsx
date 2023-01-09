d99d28858bdecd9b0be694814d818ee79c5fd955
Ajaydeep123 <rajputajaydeepsingh@gmail.com>
2023-01-10 04:04:45 +0530

    separately created for deployement

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,16 @@
+import { Stack } from "@mui/material";
+import { Link } from "react-router-dom";
+
+import { logo } from "../utils/constants";
+import { SearchBar } from "./";
+
+const Navbar = () => (
+  <Stack direction="row" alignItems="center" p={2} sx={{ position:  "sticky", background: 'linear-gradient(180.2deg, rgb(30, 33, 48) 6.8%, rgb(74, 98, 110) 131%)', top: 0, justifyContent: "space-between" }}>
+    <Link to="/" style={{ display: "flex", alignItems: "center" }}>
+      <img src={logo} alt="logo" height={45} />
+    </Link>
+    <SearchBar />
+  </Stack>
+);
+
+export default Navbar;
